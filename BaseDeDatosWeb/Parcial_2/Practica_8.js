94432ce6bedd45631981ece069f87377548ddde8
FierroEithan26 <159593278+FierroEithan26@users.noreply.github.com>
2024-10-08 18:21:21 -0700

    08/10/2024 Fierro Ibarra Eithan A.

---
+++ BaseDeDatosWeb/Parcial_2/Practica_8.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+    alert("SI JALA LA CHINGADERA")
+
+    $('#boton').on("click", function(){
+        var nombre = $('#nombre').val();
+    
+        alert(nombre);
+    })
+    $('#texto').click(function(){
+        $('#texto').css("display", "inherit").fadeOut(2000);
+    })
+})
+
